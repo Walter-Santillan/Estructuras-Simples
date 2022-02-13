e4b035a4d794e4cf26ae996826e91b5907521419
walter-santillan <waltersantillan@disroot.org>
2022-02-12 23:42:46 -0300

    Agregando nombre del autor y título

--- 9) Estructuras Repetitivas While/20220212-33.py
+++ 9) Estructuras Repetitivas While/20220212-33.py
@@ -1,6 +1,12 @@
 #33) Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc. 
 # (No se ingresan valores por teclado).
 
+print("###############################################")
+print("########\tWalter Santillán    ###########")
+print("###############################################")
+
+print("\nSerie\n")
+
 x = 1
 serie = 11
 
