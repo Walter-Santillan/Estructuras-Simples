0eb0cb518bcb4800834c14d5da977ef39ac4d645
walter-santillan <waltersantillan@disroot.org>
2022-02-12 23:44:58 -0300

    Agregando nombre del autor y título

--- 9) Estructuras Repetitivas While/20220212-34.py
+++ 9) Estructuras Repetitivas While/20220212-34.py
@@ -1,5 +1,11 @@
 # 34) Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8 - 16 - 24, etc.
 
+print("###############################################")
+print("########\tWalter Santillán    ###########")
+print("###############################################")
+
+print("\nMúltiplos\n")
+
 mult8 = 8
 
 while mult8 <= 500:
