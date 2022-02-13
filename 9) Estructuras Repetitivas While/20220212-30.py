8503b4afc4fd3f25ff161c86f0f3b3194aea9c53
walter-santillan <waltersantillan@disroot.org>
2022-02-12 23:31:24 -0300

    Agregando nombre del autor y título

--- 9) Estructuras Repetitivas While/20220212-30.py
+++ 9) Estructuras Repetitivas While/20220212-30.py
@@ -2,6 +2,12 @@
 # alumnos y nos informe cuántos tienen notas mayores o 
 # iguales a 7 y cuántos menores.
 
+print("###############################################")
+print("########\tWalter Santillán    ###########")
+print("###############################################")
+
+print("Programa que solicita las notas de les alumnes\n")
+
 x = 1
 notasAltas = 0
 notasBajas = 0
@@ -13,7 +19,7 @@ while x <= 10:
     else:
         notasBajas = notasBajas + 1
     x = x + 1
-print("La cantidad de notas mayores o igulaes a 7 es: ", notasAltas)
+print("\nLa cantidad de notas mayores o igulaes a 7 es: ", notasAltas)
 print("La cantidad de notas menores o igulaes a 7 es: ", notasBajas)
 
 
