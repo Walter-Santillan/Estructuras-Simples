ac2ea68dffaecda1d230421d38402705f3654093
walter-santillan <waltersantillan@disroot.org>
2022-02-12 23:38:34 -0300

    Agregando nombre del autor, título y ordenando el código

--- 9) Estructuras Repetitivas While/20220212-32.py
+++ 9) Estructuras Repetitivas While/20220212-32.py
@@ -3,12 +3,18 @@
 # cuántos empleados cobran entre $100 y $300 y cuántos cobran más de $300. 
 # Además el programa deberá informar el importe que gasta la empresa en sueldos al personal.
 
+print("###############################################")
+print("########\tWalter Santillán    ###########")
+print("###############################################")
+
+print("\nAnalizando sueldos de les empleades")
+
 x = 1
 cont1 = 0 #contador de empleados que cobran entre 100 y 300. 
 cont2 = 0 #contador de empleados que cobran mas de 300.
 gastoSueldos = 0 #acumulamos los suledos.
 
-n = int(input("Ingresar la cantidad de empleados: "))
+n = int(input("\nIngresar la cantidad de empleados: "))
 
 while x <= n:
     sueldos = float(input("Ingresar los sueldos de cada empleado: "))
@@ -19,6 +25,6 @@ while x <= n:
     gastoSueldos = gastoSueldos + sueldos
     x = x + 1
 
-print("Los empleados que cobran entre 100 y 300 es: ", cont1)
+print("\nLos empleados que cobran entre 100 y 300 es: ", cont1)
 print("Los empleados que cobran mas de 300 es: ", cont2)
 print("El importe que gasta la empresa en sueldos es. ", gastoSueldos)
