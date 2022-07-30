4830e279e64ed8a827a869e54a930063862d1744
walter-santillan <waltersantillan@disroot.org>
2022-07-29 22:40:39 -0300

    Valor que se encuentra en un cierto rango.

---
+++ 4) Ej Varios/ejPerteneceAlRango.py
@@ -0,0 +1,10 @@
+# VALOR QUE SE ENCUNTRA EN UN CIERTO RANGO
+print("\t\n<< PROGRAMA SI PERTENECE AL RANGO >>")
+numero = 5
+if numero > 0 and numero < 10:
+    print("\nEl numero pertenece al rango")
+else:
+    print("\nEl numero no pertenece al rango")
+    
+
+
