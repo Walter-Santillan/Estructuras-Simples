a7ace0f0673e88b40f41e33749f920f9bfb28e7a
walter-santillan <waltersantillan@disroot.org>
2022-07-29 22:38:04 -0300

    Suma avanzada de dos variables.

---
+++ 4) Ej Varios/ejSumaAvanzada.py
@@ -0,0 +1,27 @@
+print("\n***********************************************")
+print("\tSUMA AVANZADA")
+print("***********************************************")
+
+variable_1 = int(input("\nIngresar el primer valor: "))
+variable_2 = int(input("Ingresar el segundo valor: "))
+
+suma = variable_1 + variable_2
+
+print("\nLa suma es: ", suma)
+
+print("\n#############################################\n")
+
+print("***********************************************")
+print("\tOBTENER EL MAYOR\n")
+print("***********************************************")
+
+num_1 = int(input("\nIngresar el primer numero: "))
+num_2 = int(input("Ingresar el segundo numero: "))
+
+if num_1 == num_2:
+    print("\nLos numeros son iguales")
+elif num_1 > num_2:
+    print("\nEl mayor es el primer numero")
+else:
+    print("\nEl mayor es el segundo numero")
+    
