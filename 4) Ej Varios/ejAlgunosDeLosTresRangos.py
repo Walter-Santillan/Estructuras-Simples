71ce3552ea913c9004c89484704ca790fff61aa8
walter-santillan <waltersantillan@disroot.org>
2022-08-09 10:59:46 -0300

    Programa que chequea si el valor se encuentra en alguno de los tres rangos.

---
+++ 4) Ej Varios/ejAlgunosDeLosTresRangos.py
@@ -0,0 +1,28 @@
+print('\n********************************************')
+
+
+numero = 5
+if (numero > 0 and numero < 10):
+    print('\nEl numero se encuentra entre 0 y 10')
+elif (numero > 10 and numero < 20):
+    print('El numero se encuentra entre 10 y 20')
+elif (numero > 20 and numero < 30):
+    print('El numero se encuntra entre 20 y 30')
+else:
+    print('El numero no pertenece al rango')
+print('\n********************************************')
+
+print('\n#############################################\n')
+
+numero = int(input('Ingresar un numero: '))
+if (numero > 0 and numero < 10):
+    print('\nEl numero se encuentra entre 0 y 10')
+elif (numero > 10 and numero < 20):
+    print('\nEl numero se encuentra entre 10 y 20')
+elif (numero > 20 and numero < 30):
+    print('\nEl numero se encuentra entre 20 y 30')
+else:
+    print('\nNumero fuera de rango')
+    
+
+    
