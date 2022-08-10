715100e89ff807159f5cc8563bf8e17398d9a613
walter-santillan <waltersantillan@disroot.org>
2022-08-09 22:34:04 -0300

    Dos formas de mostrar los nros pares usando el bucle for.

---
+++ 4) Ej Varios/ejnrosPares_1-100.py
@@ -0,0 +1,19 @@
+print('\n')
+print('#########################################################\n')
+
+print('\nMostrar los nros pares entre 1 y 100.')
+
+print('# 1 FORMA\n')
+for i in range(1, 101):
+    if((i % 2) == 0):
+        print(i)
+        
+print("")
+
+print('\n')
+print('########################################################\n')
+
+print('# 2 FORMA\n')
+for i in range(2, 101, 2):
+    print(i)
+
